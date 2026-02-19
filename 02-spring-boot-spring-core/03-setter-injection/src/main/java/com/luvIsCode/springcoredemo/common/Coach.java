aee62cf1d0c69ae19a8d0b6df65fae2639445e8d
Animesh Anand <animeshanand705@gmail.com>
2026-02-19 09:07:30 +0530

    implementing setter injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luvIsCode/springcoredemo/common/Coach.java
@@ -0,0 +1,5 @@
+package com.luvIsCode.springcoredemo.common;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luvIsCode/springcoredemo/rest/DemoController.java
@@ -0,0 +1,31 @@
+package com.luvIsCode.springcoredemo.rest;
+
+import com.luvIsCode.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for dependencies
+    private Coach myCoach;
+
+    /*
+// constructor injection
+    @Autowired
+    public DemoController(Coach theCoach)
+    {
+        myCoach = theCoach;
+    }
+     */
+
+    @Autowired
+    public void setCoach(Coach theCoach){
+        myCoach= theCoach;
+    }
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout() {
+
+        return myCoach.getDailyWorkout();
+    }
+}
