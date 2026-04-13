bb5b3f9dafa13698dc9c645bf8bb4ace85f38da1
Animesh Anand <animeshanand705@gmail.com>
2026-04-13 17:11:28 +0530

    added the show and process form to our helloworld controller -181

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luvIsCode/springboot/thymeleafdemo/controller/demoController.java
@@ -0,0 +1,4 @@
+package com.luvIsCode.springboot.thymeleafdemo.controller;
+
+public class demoController {
+}
---
+++ 06-spring-boot-spring-mvc/02-reading-html-form-data/src/main/java/com/luvIsCode/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.luvIsCode.springboot.thymeleafdemo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    //add a controller method to show the form
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    //add a controller method to process the form
+    @RequestMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+
+}
