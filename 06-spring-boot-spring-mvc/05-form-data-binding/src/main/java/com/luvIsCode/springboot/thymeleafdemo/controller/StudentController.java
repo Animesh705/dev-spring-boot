213dba2eb6ac2788e2dc60eb0de3a4bffd3d08dd
Animesh Anand <animeshanand705@gmail.com>
2026-04-15 05:06:21 +0530

    Added drop down list for country

--- 06-spring-boot-spring-mvc/05-form-data-binding/src/main/java/com/luvIsCode/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/05-form-data-binding/src/main/java/com/luvIsCode/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,22 @@
 package com.luvIsCode.springboot.thymeleafdemo.controller;
 
 import com.luvIsCode.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    //add countries from application.properties file , instead of hard coding them
+    @Value("${countries}")
+    private List<String> countries;
+
     //first create a method for actually showing the form
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
@@ -16,6 +24,8 @@ public class StudentController {
         Student theStudent = new Student();
         //add student object to the model
         theModel.addAttribute("student",theStudent);
+      //adding countries name to the model
+        theModel.addAttribute("countries",countries);
 
         return "student-form";
     }
--- 06-spring-boot-spring-mvc/05-form-data-binding/src/main/java/com/luvIsCode/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/05-form-data-binding/src/main/java/com/luvIsCode/springboot/thymeleafdemo/model/Student.java
@@ -6,6 +6,16 @@ public class Student {
 
     private String lastName;
 
+    private String country;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     public String getFirstName() {
         return firstName;
     }
