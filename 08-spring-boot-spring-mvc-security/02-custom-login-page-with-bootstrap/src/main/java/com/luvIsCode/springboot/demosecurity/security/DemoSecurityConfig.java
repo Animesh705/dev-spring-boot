251a4ff88ed2012aaa8362a679ed13a23c607547
Animesh Anand <animeshanand705@gmail.com>
2026-04-20 09:02:05 +0530

    added bootStrap styling to the custom login page

---
+++ 08-spring-boot-spring-mvc-security/02-custom-login-page-with-bootstrap/src/main/java/com/luvIsCode/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.luvIsCode.springboot.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails jhon= User.builder()
+                .username("jhon")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary= User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan= User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(jhon,mary,susan);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configure ->
+                configure.anyRequest().authenticated()
+        )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                );
+        return http.build();
+    }
+}
